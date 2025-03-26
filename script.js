a2a5e538ff5f598c4dbf7bf1cea849f7d0ab158b
jax-screech <jeromepaddy7@gmail.com>
2025-03-26 10:43:00 +0300

    worked on footer

---
+++ script.js
@@ -0,0 +1,8 @@
+function onSubmit(){
+    alert ("Form submitted succesfully")
+    return true;
+}
+function welcomeMsg(name) {
+    name = "user";
+    return ("hello " + name + "!" + " welcome to my website!");
+}
